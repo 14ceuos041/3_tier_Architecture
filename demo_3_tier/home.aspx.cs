ec4d4b7f1d4238dc14c6c01d318628dfe8ff1f4d
jhbaxi <baxi099@gmail.com>
2016-09-21 12:14:56 +0530

    Added 3 tier Architecture demo App

---
+++ demo_3_tier/home.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using logic;
+
+namespace demo_3_tier
+{
+    public partial class home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            logicclass c = new logicclass();
+            c.savetodata(TextBox1.Text);
+        }
+    }
+}
